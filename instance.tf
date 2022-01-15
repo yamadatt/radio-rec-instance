694071ef51474d03ce83e9718dc4631a708162e0
yamadatt <yamadatt@gmail.com>
2022-01-15 13:30:32 +0900

    #3 security-groupを別ファイルに分割した

--- instance.tf
+++ instance.tf
@@ -33,36 +33,3 @@ output "server_public_ip" {
   value       = aws_instance.raido-rec.public_ip
 }
 
-# Security Group
-
-resource "aws_security_group" "radio_sg" {
-  name   = "ec2-sg"
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "ec2-sg"
-  }
-}
-
-variable "allowed_cidr" {
-  default = null
-}
-
-data "http" "ipify" {
-  url = "http://api.ipify.org"
-}
-
-locals {
-  myip         = chomp(data.http.ipify.body)
-  allowed_cidr = (var.allowed_cidr == null) ? "${local.myip}/32" : var.allowed_cidr
-}
-
-resource "aws_security_group_rule" "in_ssh" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = [local.allowed_cidr]
-  security_group_id = aws_security_group.radio_sg.id
-}
-
